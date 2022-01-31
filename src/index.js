0a123293bde8b2ebf27b81a2e890849838742eab
DescrySama <pierrehugot35@gmail.com>
2022-01-31 11:32:40 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+class Promo {
+    constructor(nom, prenom, age, email, numero, note) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+        this.email = email;
+        this.numero = numero;
+        this.note = note;
+    }
+ 
+ addNotePoint(){
+     return this.note += 6
+ }
+}
 
-import confetti from 'canvas-confetti';
+let Erwin = new Promo('gossin', 'erwin', '19', 'erwin@gmail.com', '0674859384', 6);
+let Esteban = new Promo('girard', 'esteban', '18', 'esteban@gmail.com', '0674958424', 13);
 
-confetti.create(document.getElementById('canvas'), {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+console.log('La note de ' + Esteban.prenom + ' est de ' + Esteban.note);
+
+console.log('Apres la reverification de son devoir la note de ' + Esteban.prenom + ' est de ' + Esteban.addNotePoint());
+
+console.log('La note de ' + Erwin.prenom + ' est de ' + Erwin.note);
+
+console.log('Apres la reverification de son devoir la note de ' + Erwin.prenom + ' est de ' + Erwin.addNotePoint());
